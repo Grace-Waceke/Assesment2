f9b096a73ba840d6cb3f415fb643d4543c438fe8
Grace Waceke <gracewacekemwangi@gmail.com>
2022-04-14 09:28:02 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,35 @@
+import jdk.jfr.Category
+import java.time.temporal.TemporalAmount
+
+fun main(){
+   var x= currentAccount("2345667", "Grace", 3000)
+   println(x.balance)
+fruit("banana")
+}
+data class currentAccount(var accountNumber:String, var accName:String, var balance:Int)
+  fun deposit(amount:Double):String{
+     var sum=amount
+      //var increament= amount+ deposit
+     println(sum)
+ }
+ fun withdraw(amount: Double){
+
+ }
+fun fruit(sweet:String):String{
+    var tunda="banana"
+    sweet.forEach { lol->
+    println(lol.toString()+[0]+sweet[2]+sweet[4])
+        println(tunda)
+    }
+
+
+    return ("tunda")
+}
+//data class big(var name:String, var weight:Double, var price:Int, var category: String)
+//    fun product(name:String):Boolean{
+//        if (big is big)
+
+
+
+
+
