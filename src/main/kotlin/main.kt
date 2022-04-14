bf42ec5dc831afd999e5025b6c2e2c392fffc3f9
Grace Waceke <gracewacekemwangi@gmail.com>
2022-04-14 10:57:14 +0300

    first commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,33 +2,55 @@ import jdk.jfr.Category
 import java.time.temporal.TemporalAmount
 
 fun main(){
-   var x= currentAccount("2345667", "Grace", 3000)
-   println(x.balance)
-fruit("banana")
-}
-data class currentAccount(var accountNumber:String, var accName:String, var balance:Int)
-  fun deposit(amount:Double):String{
-     var sum=amount
-      //var increament= amount+ deposit
-     println(sum)
- }
- fun withdraw(amount: Double){
+    var waceke=currentAccount("234556","grace",90000.00)
+    waceke.deposit(500.00)
+   println( waceke.balance)
+    waceke.withdrawal(4000.00)
+    println( waceke.balance)
+    fruit("banana")
 
- }
+}
+class currentAccount(var accountNumber:String, var accName:String, var balance:Double){
+
+    fun deposit(amount:Double){
+        var increase=balance
+        balance+=amount
+        println(amount)
+
+    }
+    fun withdrawal(amount: Double){
+        var decrease=balance
+        balance-=amount
+        println(amount)
+
+    }
+}
+
+//     var sum=amount
+//      //var increament= amount+ deposit
+//     println(sum)
+// }
+// fun withdraw(amount: Double){
+//
+// }
 fun fruit(sweet:String):String{
-    var tunda="banana"
-    sweet.forEach { lol->
-    println(lol.toString()+[0]+sweet[2]+sweet[4])
+    var tunda=""
+    sweet.forEachIndexed { index, c ->
+        if (index%2==0){
+            tunda+=c
+        }
         println(tunda)
     }
-
-
-    return ("tunda")
+return tunda
 }
-//data class big(var name:String, var weight:Double, var price:Int, var category: String)
-//    fun product(name:String):Boolean{
-//        if (big is big)
 
+data class big(var name:String, var weight:Double, var price:Int, var category: String)
+fun product(num:Int):Boolean {
+    return (num % 2 == 0)
+    if ((num % 2 == 0)) {
+    }
+    return true
+}
 
 
 
